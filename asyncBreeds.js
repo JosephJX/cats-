45267612c172d463750a803cd5ff840fb99eb2c5
Joseph McGregor <joseph00@my.yorku.ca>
2021-12-09 09:23:43 -0500

    cat breeds

---
+++ asyncBreeds.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    
+    if (!error) functionToRunWhenThingsAreDone(data);
+  });
+};
+
+const prinOutCatBreed = breed => {
+  console.log('Return Value: ', breed)
+};
+
+breedDetailsFromFile('Bombay', prinOutCatBreed);
